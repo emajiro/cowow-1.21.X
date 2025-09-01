84023f8609551a446ecca33682b247540589b068
emajiro <emuwu@proton.me>
2025-09-01 10:09:54 -0500

    important message

--- src/main/java/net/emajiro/cowow/Cowow.java
+++ src/main/java/net/emajiro/cowow/Cowow.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//comment
 public class Cowow implements ModInitializer {
 	public static final String MOD_ID = "cowow";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
